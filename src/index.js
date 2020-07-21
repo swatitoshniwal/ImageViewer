95266fc9f5150649054701f07bcaadb01b706d2f
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-07-21 15:50:53 +0530

    Username and Password for login page created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './common/header/Header';
+//import Header from './common/header/Header';
 import * as serviceWorker from './serviceWorker';
+import Login from './screens/login/Login'
 
 ReactDOM.render(
-    <Header/>,document.getElementById('root')
+    <Login/>,document.getElementById('root')
 );
 
 serviceWorker.unregister();
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -3,10 +3,19 @@ import Header from '../../common/header/Header';
 import './Login.css';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
 const styles = {
     card:{
-
+        padding: '15px',
+        position: 'relative',
+        top: '90px',
+        left: '50%',
+        width: '325px',
+        transform: 'translateX(-50%)',
     }
 }
 
@@ -17,7 +26,15 @@ class Login extends Component{
                 <Header screen={"Login"}/>
                 <Card style={styles.card}>
                     <CardContent>
-
+                        <Typography style={styles.title}> LOGIN </Typography><br />
+                        <FormControl>
+                            <InputLabel htmlFor="username"> Username </InputLabel>
+                            <Input id="username" type="text" username={this.state.username}/>
+                        </FormControl><br/><br/>
+                        <FormControl>
+                            <InputLabel htmlFor="password"> Username </InputLabel>
+                            <Input id="password" type="password"/>
+                        </FormControl><br/><br/>
                     </CardContent>
                 </Card>
             </div>
