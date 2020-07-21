19e36491592ef6e14d043522b7d488655408409c
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-07-21 13:09:37 +0530

    Login Card created

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import Header from '../../common/header/Header';
-import './Login.css'
+import './Login.css';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const styles = {
+    card:{
+
+    }
+}
 
 class Login extends Component{
     render(){
         return(
-            <Header screen
+            <div className= "main-container">
+                <Header screen={"Login"}/>
+                <Card style={styles.card}>
+                    <CardContent>
+
+                    </CardContent>
+                </Card>
+            </div>
+            
         )
     }
 }
+
+export default Login;
