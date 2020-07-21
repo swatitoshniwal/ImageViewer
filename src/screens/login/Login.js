7c8c3b70c6af461d23a1dfabcf98bc41c93b22aa
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-07-21 12:56:09 +0530

    Login.js created

---
+++ src/screens/login/Login.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Header from '../../common/header/Header';
+import './Login.css'
+
+class Login extends Component{
+    render(){
+        return(
+            <Header screen
+        )
+    }
+}
