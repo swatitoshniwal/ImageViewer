c3c2d3551d364d239e9add8461a19543045bfb21
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-07-22 20:46:05 +0530

    Header for Home page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import './Header.css';
 import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import { Toolbar } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
 const styles = theme => ({
     appheader:{
@@ -14,7 +16,37 @@ const styles = theme => ({
         backgroundColor: '#c0c0c0',
         marginLeft: 0,
         width: '300px',
-    }
+    },
+
+    grow:{
+        flexGrow: 1
+    },
+
+    searchIcon: {
+        width: theme.spacing(4),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color:'#000000'
+      },
+
+      inputInput: {
+        paddingTop: theme.spacing(1),
+        paddingRight: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+        paddingLeft: theme.spacing(4),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: 120,
+          '&:focus': {
+            width: 200
+          }
+        }
+      },
 })
 
 class Header extends Component{
@@ -29,12 +61,22 @@ class Header extends Component{
         const {classes,screen} = this.props;
         return(
             <div> 
-                <Toolbar>
-                    <AppBar className={classes.appheader}> 
-                    {(screen === "Login" || screen === "Home") && <span className="header-logo">Image Viewer</span>} 
-                    <div className={classes.search}></div>
-                    </AppBar>
-                </Toolbar>
+                <AppBar className={classes.appheader}> 
+                    <Toolbar>
+                        <div className={classes.grow}/>
+                        {(screen === "Login" || screen === "Home") && <span className="header-logo">Image Viewer</span>} 
+                        {(screen === "Home") &&
+                            <div className={classes.search}>
+                                <div className={classes.searchIcon}>
+                                    <SearchIcon/>
+                                </div>
+                                <InputBase onChange={(e)=>{this.props.searchHandler(e.target.value)}} placeholder="Search…" classes={{
+                                input: classes.inputInput
+                                }}/>
+                            </div>
+                        }
+                    </Toolbar>
+                </AppBar>    
             </div>
         )
     }
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -5,21 +5,53 @@ import {withStyles} from '@material-ui/core/styles';
 import {constant} from '../../common/utils'
 
 const styles = theme => ({
-
-})
+    card: {
+        maxWidth: 1100,
+      },
+});
 
 class Home extends Component{
     constructor(props){
         super(props);
+        if(sessionStorage.getItem('access-token')==null){
+            props.history.replace('/');
+        }
         this.state = {
+            data: [],
+            filteredData:[],
+            userData:{},
 
         }
     }
+
+    onSearchEntered= (value) =>{
+        console.log('search value', value);
+        let filteredData = this.state.data;
+        filteredData = filteredData.filter((data) =>{
+          let string = data.caption.text.toLowerCase();
+          let subString = value.toLowerCase();
+          return string.includes(subString);
+        })
+        this.setState({
+          filteredData
+        })
+      }
+
+    logout = () => {
+        sessionStorage.clear();
+        this.props.history.replace('/');
+    }
+    
+    navigateToAccount = () =>{
+        this.props.history.push('/profile');
+      }
+
     render(){
         const{classes} = this.props;
         return(
             <div>
                 <Header
+                    userProfileUrl={this.state.userData.profile_picture}
                     screen={"Home"}
                     searchHandler={this.onSearchEntered}
                     handleLogout={this.logout}
@@ -30,4 +62,4 @@ class Home extends Component{
     }
 }
 
-export default withStyles(styles)(Home);
+export default withStyles(styles)(Home)
